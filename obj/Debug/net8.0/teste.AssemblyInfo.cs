49659bb7ef8e2647f9c7076539753f7f09157b0f
Antônio de Pádua <antonyejoao@gmail.com>
2024-04-26 12:23:59 -0300

    commitando novas alterações

--- obj/Debug/net8.0/teste.AssemblyInfo.cs
+++ obj/Debug/net8.0/teste.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("teste")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29b4f2710736a20ec37133283dc6dfdea69e454e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b120216d76c54e81e66a1de497e3a929ccf8880")]
 [assembly: System.Reflection.AssemblyProductAttribute("teste")]
 [assembly: System.Reflection.AssemblyTitleAttribute("teste")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
